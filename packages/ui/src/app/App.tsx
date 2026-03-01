eee69bf47be85da38acb9129b3af3a78191c8315
caiyongji <i@caiyongji.com>
2026-03-02 00:20:55 +0800

    font offline

--- packages/ui/src/app/App.tsx
+++ packages/ui/src/app/App.tsx
@@ -1,3 +1,8 @@
+import '@fontsource/press-start-2p'
+import '@fontsource/jetbrains-mono/400.css'
+import '@fontsource/jetbrains-mono/500.css'
+import '@fontsource/jetbrains-mono/600.css'
+import '@fontsource/jetbrains-mono/700.css'
 import '../styles/global.css'
 import '../i18n/config'
 import { ErrorBoundary } from '../components'
